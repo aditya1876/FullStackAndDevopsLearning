93067c7c2f56d4af78a97f62c4b39037f9214f54
adi <adikakaam@gmail.com>
2024-08-25 17:23:16 +0530

    Week 3 class 1 done

--- 02_DOM/TodoApp_complex/myScript.js
+++ 02_DOM/TodoApp_complex/myScript.js
@@ -1,20 +1,95 @@
 console.log("RUNNING MY TODO APP - V3");
 
 let counter = 0;
+let todoList = [];
 
+// function addTodo() {
+//   const todoText = document.querySelector("input").value;
+//
+//   const spanEle = document.createElement("span");
+//   spanEle.innerHTML = todoText;
+//   const buttonEle = document.createElement("button");
+//   buttonEle.innerHTML = "Delete";
+//   const divEle = document.createElement("div");
+//
+//   divEle.appendChild(spanEle);
+//   divEle.appendChild(buttonEle);
+//
+//   document.querySelector("body").appendChild(divEle);
+//
+//   console.log("Adding a Todo item with text: " + todoText);
+// }
+
+//Create HTML dynamically and display all Todos
+function renderTodos() {
+  let todoText;
+  let spanEle, buttonEle, divEle;
+
+  //remove all todos before rendering again otherwise todos will get appeded to existing data
+  const wrapperDiv = document.querySelector("#wrapper");
+  wrapperDiv.parentNode.removeChild(wrapperDiv);
+
+  //create new wrapper element
+  todowrapper = document.createElement("div");
+  todowrapper.setAttribute("id", "wrapper");
+
+  for (let i = 1; i <= todoList.length; i++) {
+    todoText = todoList[i - 1];
+
+    //create html elements to display
+    spanEle = document.createElement("span");
+    buttonEle = document.createElement("button");
+    divEle = document.createElement("div");
+
+    //add attributes to the elements
+    spanEle.setAttribute("id", "span_" + i);
+    spanEle.innerHTML = todoText;
+    buttonEle.setAttribute("id", "button_" + i);
+    buttonEle.setAttribute("onclick", "deleteTodo(todo_" + i + ")");
+    buttonEle.innerHTML = "Delete";
+    divEle.setAttribute("id", "todo_" + i);
+
+    //create structure
+    divEle.appendChild(spanEle);
+    divEle.appendChild(buttonEle);
+    todowrapper.appendChild(divEle);
+  }
+
+  //append all todos to document
+  document.querySelector("#all_todos").appendChild(todowrapper);
+
+  console.log("Displaying all todos. Total todos: [" + todoList.length + "].");
+}
+
+//Function to add a new Todo
 function addTodo() {
+  //capture the todo content from UI.
   const todoText = document.querySelector("input").value;
 
-  const spanEle = document.createElement("span");
-  spanEle.innerHTML = todoText;
-  const buttonEle = document.createElement("button");
-  buttonEle.innerHTML = "Delete";
-  const divEle = document.createElement("div");
+  //add to the list of todos
+  todoList.push(todoText);
 
-  divEle.appendChild(spanEle);
-  divEle.appendChild(buttonEle);
+  console.log("Added new todo: [" + todoText + "]");
 
-  document.querySelector("body").appendChild(divEle);
-
-  console.log("Adding a Todo item with text: " + todoText);
+  //now render the complete list on UI
+  renderTodos();
 }
+
+//Function to delete a todo
+function deleteTodo(todo_id) {
+  //delete the todo from the list
+  const id = String(todo_id).split("_")[1];
+  todoList.splice(id, 1);
+
+  console.log("Deleted todo item: [" + todoList[id] + "]");
+
+  //render all todos
+  renderTodos();
+}
+
+///Function to Edit the todo
+//function editTodo(todo_id){
+//  const itemToEdit = document.getElementById(todo_id);
+//
+//
+//}
--- Notes/MyNotes.md
+++ Notes/MyNotes.md
@@ -65,3 +65,39 @@ setTimeoutPromisified(3000).then(functionToCallback);
 ![Dom_Structure](./images/DOM_01.png);
 
 ## NODEJS
+
+- It is an open-source JS runtime that allows to execute js code outside of a browser (or in a server).
+- Build on Chrome's V8 engine
+- Browser engines to run js:
+  - chrome - V8
+  - firefox - spidermonkey
+  - safari - javascriptCore
+- BUN
+  - Alternate to Node.js (it is also a js runtime.)
+  - Completely built from ground up.
+- Intializing a project in Node.js
+  - create project folder
+  - navigate inside folder
+  - run - `npm init -y` (you should see some message for package.json)
+    - Package.json:
+      - name - name of the project
+      - version - versioning
+        - Format - MAJOR.MINOR.PATCH
+          - MAJOR: major version changes or breaking changes from previous version.
+          - MINOR: New feature addition or improvements in backward compatible manner.
+          - PATCH: Backward compatible bug fixes or improvements.
+        - The `^` sign in-front: it means npm will install any version compatible with this version (eg - "^5.3.1" means it can install any version from 5.3.1 to 5.9.9999 but not 6.0.0 because it will be a breaking change)
+      - main - which file should be the entry point for the code.
+      - scripts - developer specified scripts.
+        - Example- Add `"start": "node index.js",` to scripts section. Now you can run `npm run start` in terminal to execute node index.js.
+      - keywords - Metadata
+      - author - Metadata
+      - license - Metadata
+      - description - Metadata
+      - dependencies - Lists the packages required for the project to run. As the project user installs new packages, it gets added to this section
+    - package.json-lock: even though, package.json provides a compatible version to install, sometimes minor version change between 2 developer machines/ local and server may arise due to package updates. This may cause code failures for our project. Hence, lock will automatically, lock to a single version of packages for whoever installs the project locally.
+  - open the folder in code editor
+  - create `index.js` in this folder
+  - Installing external package - `npm install <package name>`
+    - This adds a folder `node_modules` in the project folder. node_modules contains the code and dependencies of the package itself.
+  - Setup project - `npm install`
